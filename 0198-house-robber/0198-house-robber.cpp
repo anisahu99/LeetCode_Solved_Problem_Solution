a8bc2a983a478464ebd3c6d99e036d3c1ec10f03
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-05-08 10:27:07 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (97.18%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    int dp[101];
-    int solve(int i,vector<int>& nums){
-        if(i>=nums.size())
-            return 0;
-        if(dp[i]!=-1){
-            //cout<<dp[i]<<endl;
-            return dp[i];
-        }
-        int take=nums[i]+solve(i+2,nums);
-        int not_take=0+solve(i+1,nums);
-        
-        return dp[i]=max(take,not_take);
-    }
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,nums);
+        int n=nums.size();
+        int take=nums[0];
+        int not_take=0;
+        for(int i=1;i<n;i++){
+            int newTake=not_take+nums[i];
+            int newNotTake=max(take,not_take);
+            take=newTake;
+            not_take=newNotTake;
+        }
+        return max(take,not_take);
     }
 };
