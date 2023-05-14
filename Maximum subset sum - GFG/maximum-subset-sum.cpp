0ed174972a70bb8ad1e3d6c12d77860a709aa123
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-05-15 01:00:49 +0530

    Added solution - LeetHub

--- Maximum subset sum - GFG/maximum-subset-sum.cpp
+++ Maximum subset sum - GFG/maximum-subset-sum.cpp
@@ -47,22 +47,22 @@ class Solution {
   
   #define ll long long
   ll solve(int i,int f,vector<int>&A,vector<vector<ll>>&dp){
-      if(i<0){
+      if(i>=A.size()){
           return 0;
       }
       if(dp[i][f]!=-1)return dp[i][f];
       if(f==1){
-          return dp[i][f]=max(A[i]+solve(i-1,1,A,dp),solve(i-1,0,A,dp));
+          return dp[i][f]=max(A[i]+solve(i+1,1,A,dp),solve(i+1,0,A,dp));
       }
       else{
-          return dp[i][f]=A[i]+solve(i-1,1,A,dp);
+          return dp[i][f]=A[i]+solve(i+1,1,A,dp);
       }
       
   }
     long long findMaxSubsetSum(int N, vector<int> &A) {
         // code here
         vector<vector<ll>>dp(N,vector<ll>(2,-1));
-        return max(solve(N-1,0,A,dp),solve(N-1,1,A,dp));
+        return max(solve(1,0,A,dp),A[0]+solve(1,1,A,dp));
     }
 };
 
