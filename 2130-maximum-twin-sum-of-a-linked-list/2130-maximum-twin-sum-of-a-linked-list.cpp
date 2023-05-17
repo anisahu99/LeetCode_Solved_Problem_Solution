901428c0a265d42ef127d8ccefbd1d6d701b1b64
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-05-17 11:35:21 +0530

    Time: 280 ms (32.82%), Space: 116.7 MB (75.42%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -27,29 +27,20 @@ public:
             return temp1;;
     }
     int pairSum(ListNode* head) {
-        ListNode* temp=head;
-        int n=0;
-        while(temp){
-            n++;
-            temp=temp->next;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast&&fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        int i=0;
-        temp=head;
-        while(i!=(n/2-1)){
-            temp=temp->next;
-            i++;
-        }
-        temp->next=reverseList(temp->next);
-        
-        ListNode* ptr1=head;
-        ListNode* ptr2=temp->next;
+        fast=reverseList(slow);
+        slow=head;
         int ans=0;
-        while(ptr2){
-            ans=max(ans,(ptr1->val+ptr2->val));
-            ptr1=ptr1->next;
-            ptr2=ptr2->next;
+        while(fast){
+            ans=max(ans,(slow->val+fast->val));
+            slow=slow->next;
+            fast=fast->next;
         }
-        
         return ans;
     }
 };
