dbd54fa77cc3f7a24cc4a44d83faee07072957b7
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-05-17 11:25:28 +0530

    Time: 283 ms (27.34%), Space: 116.6 MB (75.42%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,17 +10,46 @@
  */
 class Solution {
 public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !(head -> next)) {//if no node present !head
+            return head;
+        }
+        ListNode*temp=head->next;
+        ListNode*temp1=head;
+        ListNode*extra=temp;
+         temp1->next=NULL;   
+            while(temp!=NULL){
+                    extra=temp->next;
+                    temp->next=temp1;
+                    temp1=temp;
+                    temp=extra;
+            }
+            return temp1;;
+    }
     int pairSum(ListNode* head) {
-        vector<int> v;
-        while(head){
-            v.push_back(head->val);
-            head=head->next;
+        ListNode* temp=head;
+        int n=0;
+        while(temp){
+            n++;
+            temp=temp->next;
         }
-        int n=v.size();
+        int i=0;
+        temp=head;
+        while(i!=(n/2-1)){
+            temp=temp->next;
+            i++;
+        }
+        temp->next=reverseList(temp->next);
+        
+        ListNode* ptr1=head;
+        ListNode* ptr2=temp->next;
         int ans=0;
-        for(int i=0;i<=(n/2)-1;i++){
-            ans=max(ans,v[i]+v[n-1-i]);
+        while(ptr2){
+            ans=max(ans,(ptr1->val+ptr2->val));
+            ptr1=ptr1->next;
+            ptr2=ptr2->next;
         }
+        
         return ans;
     }
 };
