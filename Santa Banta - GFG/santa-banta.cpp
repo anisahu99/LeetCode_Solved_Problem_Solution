6af2ec95a9f06e1877220b0dbe8483a298705a13
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-06-03 12:47:04 +0530

    Added solution - LeetHub

--- Santa Banta - GFG/santa-banta.cpp
+++ Santa Banta - GFG/santa-banta.cpp
@@ -5,50 +5,86 @@ using namespace std;
 
 // } Driver Code Ends
 
-vector<int> ans;
 
+vector<int> Prime;
 class Solution{
 public:
-
-// Global things : prime array.
-// Find the graph in th forest with maximum nodes. K is the number of nodes and out = kth prime.
-    int dfs(int vertex, int ct, vector<vector<int>>&g, vector<bool>&vis){
-        vis[vertex] = true;
-        ct++;
-        for(auto & child : g[vertex]){
-            if(vis[child]) continue;
-            ct = dfs(child, ct, g, vis);
-        }
-        return ct;
-    }
-    void precompute(){
-        // precalculate all the prime numbers till 1e5 prime numbers are obtained;
-        const long long N = 2e6 + 1;
-        vector<bool>prime(N, true);
-        // cout << prime[0] << endl;
-        for(long long p = 2; p*p <=N; p++){
-            
-            if(prime[p]){
-                for(long long i = p*p; i <= N; i+=p)
-                    prime[i] = false;
-            }    
-        
-        }
-        for(long long i = 2; i<=N; i++){
-            if(prime[i] == true) ans.push_back(i);
+    
+    void precompute()
+     {
+    int n=1e6;
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+  
+    for (int p = 2; p * p <= n; p++) {
+        // If prime[p] is not changed, then it is a prime
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
         }
     }
+  
+    // Print all prime numbers
+    for (int p = 2; p <= n; p++){
+        if (prime[p]){
+            Prime.push_back(p);
+        }
+     }
+    }
+    
+    vector<int> rank1;
+    vector<int> par;
+    int find_parent(int val){
+        if(par[val]==val) return val;
+        par[val]=find_parent(par[val]);
+        return par[val];
+    }
+    void union_( int a, int b) 
+    {
+        int a_parent=find_parent(a);
+        int b_parent=find_parent(b);
+        if(a_parent==b_parent) return;
+        if(rank1[a_parent]==rank1[b_parent]){
+            par[b_parent]=a_parent;
+            rank1[a_parent]++;
+        }else{
+            if(rank1[a_parent]>rank1[b_parent]){
+                par[b_parent]=a_parent;
+            }
+            else{
+                par[a_parent]=b_parent;
+            }
+        }
+        return;
+    }
+    
+    //Function to check whether 2 nodes are connected or not.
+    
     int helpSanta(int n, int m, vector<vector<int>> &g){
-        
-        if(m==0)return-1;
-        vector<bool>vis(n+1, false);
-        
-        int ct = 0;
-        for(int i = 1; i <= n; i++){
-            if(vis[i]) continue;
-            ct = max(ct, dfs(i, 0, g, vis));
+        // Code here
+        if(m==0)
+        return -1;
+        rank1.resize(n+1);
+        par.resize(n+1);
+        for(int i=1;i<=n;i++){
+            par[i]=i;
         }
-        return ans[ct-1];
+        for(int i=1;i<=n;i++){
+            for(int &j:g[i]){
+                union_(i,j);
+            }
+        }
+        vector<int> cnt(n+1,0);
+        
+        for(int i=1;i<=n;i++){
+            cnt[par[i]]++;
+        }
+        int mx=INT_MIN;
+        for(int i=1;i<=n;i++){
+            mx=max(mx,cnt[i]);
+        }
+        return Prime[mx-1];
+        
     }
 };
 
