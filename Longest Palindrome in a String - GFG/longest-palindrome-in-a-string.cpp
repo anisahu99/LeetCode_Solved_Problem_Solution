d1f2ce1dfa9d27a8a63b98482ac55b08ad3d0556
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-06-15 10:44:33 +0530

    Added solution - LeetHub

---
+++ Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+  
+  vector<vector<int>> dp;
+    
+    bool isPalin(int left,int right,string &s){
+        if(left>=right)
+            return true;
+        
+        if(dp[left][right]!=-1)
+            return dp[left][right];
+        
+        if(s[left]!=s[right]){
+            dp[left][right]=false;
+            return false;
+        }
+        
+        //  0  1  2
+        //  b  a  b
+        
+        if(right-left<=2){
+            dp[left][right]=true;
+            return true;
+        }
+        
+        
+        return dp[left][right]=isPalin(left+1,right-1,s);
+    }
+    string longestPalin (string &s) {
+        // code here
+        int len=s.length();
+        int maxLen=0;
+        int startingIndex=-1;
+        dp.resize(len,vector<int>(len,-1));
+        for(int left=0;left<len;left++){
+            for(int right=left;right<len;right++){
+                if(s[left]==s[right]&&isPalin(left+1,right-1,s)){
+                    if(right-left+1>maxLen){
+                        maxLen=right-left+1;
+                        startingIndex=left;
+                    }
+                }
+            }
+        }
+        return s.substr(startingIndex,maxLen);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string S; cin >> S;
+        
+        Solution ob;
+        cout << ob.longestPalin (S) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
