6c7b21f3e972712a86da6a64434cb12eeedf6fcc
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-06-22 09:47:56 +0530

    Time: 41 ms (84.37%), Space: 6.6 MB (89.06%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    int solveTab(int &x, int &y, string &s1, string &s2){
+    int solveOptimal(int &x, int &y, string &s1, string &s2){
         
-        vector<vector<int>> dp(x+1,vector<int>(y+1,0));
+
+        vector<int> curr1(y+1,0);
+
+        vector<int> next1(y+1,0);
+        
         for(int i=x-1;i>=0;i--){
             for(int j=y-1;j>=0;j--){
                 int ans1=0;
                 int ans2=0;
                 int ans3=0;
                 if(s1[i]==s2[j]){
-                    ans1=1+dp[i+1][j+1];
+                    ans1=1+next1[j+1];
                 }
                 else{
-                    ans2=0+dp[i][j+1];
-                    ans3=0+dp[i+1][j];
+                    ans2=0+curr1[j+1];
+                    ans3=0+next1[j];
                 }
                 int ans=max(ans1,max(ans2,ans3));
-                dp[i][j]= ans;
+                curr1[j]= ans;
             }
+            next1=curr1;
         }
-        return dp[0][0];
+        return next1[0];
     }
     int longestCommonSubsequence(string text1, string text2) {
         
         int x=text1.length();
         int y=text2.length();
         
-        return solveTab(x,y,text1,text2);
+        return solveOptimal(x,y,text1,text2);
 
     }
 };
