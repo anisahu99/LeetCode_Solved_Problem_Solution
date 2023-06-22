ea7ab42b5b85e36f0dab5244cba2934d9b89856e
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-06-22 22:39:34 +0530

    Time: 248 ms (47.27%), Space: 90.8 MB (47.11%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -3,31 +3,35 @@ public:
     int solveTab(vector<int>& prices,int &fee){
         vector<vector<int>> dp(prices.size()+1,vector<int>(3,0));
         
+        vector<int> curr(3,0);
+        vector<int> next(3,0);
+        
         
         for(int day=prices.size()-1;day>=0;day--){
             for(int buy=0;buy<2;buy++){
                 int profit=0;
                 if(buy){
                     //        sell call
-                    int do_buy=dp[day+1][0]-prices[day]-fee;
+                    int do_buy=next[0]-prices[day]-fee;
 
-                    int not_buy=dp[day+1][1];
+                    int not_buy=next[1];
 
                     profit=max(profit,max(do_buy,not_buy));
                 }
                 else{
                     //                       1 extra day for cool down
-                    int sell=prices[day]+dp[day+1][1];
+                    int sell=prices[day]+next[1];
 
-                    int not_sell=dp[day+1][0];
+                    int not_sell=next[0];
 
                     profit=max(profit,max(sell,not_sell));
                 }
 
-                dp[day][buy]=profit;
+                curr[buy]=profit;
             }
+            next=curr;
         }
-        return dp[0][1];
+        return next[1];
         
     }
     int maxProfit(vector<int>& prices, int &fee) {
