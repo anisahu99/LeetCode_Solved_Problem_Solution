c08fd7ca86bb9159891d5461214818ad5a7db17e
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-06-22 22:43:36 +0530

    Time: 111 ms (90.59%), Space: 55 MB (77.58%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     int solveTab(vector<int>& prices,int &fee){
         
+        int curr0=0;
+        int curr1=0;
+        int next0=0;
+        int next1=0;
+        
         vector<int> curr(2,0);
         vector<int> next(2,0);
         
@@ -11,26 +16,32 @@ public:
                 int profit=0;
                 if(buy){
                     //        sell call
-                    int do_buy=next[0]-prices[day]-fee;
+                    int do_buy=next0-prices[day]-fee;
 
-                    int not_buy=next[1];
+                    int not_buy=next1;
 
                     profit=max(profit,max(do_buy,not_buy));
                 }
                 else{
                     //                       1 extra day for cool down
-                    int sell=prices[day]+next[1];
+                    int sell=prices[day]+next1;
 
-                    int not_sell=next[0];
+                    int not_sell=next0;
 
                     profit=max(profit,max(sell,not_sell));
                 }
+                
+                if(buy==1){
+                    curr1=profit;
+                }else{
+                    curr0=profit;
+                }
 
-                curr[buy]=profit;
             }
-            next=curr;
+            next0=curr0;
+            next1=curr1;
         }
-        return next[1];
+        return next1;
         
     }
     int maxProfit(vector<int>& prices, int &fee) {
