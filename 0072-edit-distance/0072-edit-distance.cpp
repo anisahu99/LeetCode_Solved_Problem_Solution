2c3e5c02e6697f0d5841dcdcb2c4bbbbd8492e38
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-06-23 10:51:03 +0530

    Time: 15 ms (63.19%), Space: 9 MB (62.81%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    int solveTab(string &s1,string &s2,int x,int y){
-        vector<vector<int>>dp(x+1,vector<int>(y+1,0));
+    vector<vector<int>>dp;
+    int solve(int i,int j,string &s1,string &s2,int &x,int &y){
         
-        for(int j=0;j<y;j++){
-            dp[x][j]=y-j;
-        }
-        for(int i=0;i<x;i++){
-            dp[i][y]=x-i;
+        if(i==x){
+            return y-j;
         }
         
-        for(int i=x-1;i>=0;i--){
-            for(int j=y-1;j>=0;j--){
-                int ans=0;
-                if(s1[i]==s2[j]){
-                    dp[i][j]=dp[i+1][j+1];
-                }
-                else{
-                    int insrt=1+dp[i][j+1];
-                    int rplc=1+dp[i+1][j+1];
-                    int delt=1+dp[i+1][j];
-                    
-                    ans=min(insrt,min(rplc,delt));
-                    dp[i][j]=ans;
-                }
-                
-            }
+        if(j==y){
+            return x-i;
         }
-        return dp[0][0];
+        
+        
+        
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        
+        int ans=0;
+        if(s1[i]==s2[j]){
+            return dp[i][j]=solve(i+1,j+1,s1,s2,x,y);
+        }
+        else{
+            int insrt=1+solve(i,j+1,s1,s2,x,y);
+            int rplc=1+solve(i+1,j+1,s1,s2,x,y);
+            int delt=1+solve(i+1,j,s1,s2,x,y);
+            
+            ans=min(insrt,min(rplc,delt));
+        }
+        return dp[i][j]=ans;
     }
     int minDistance(string word1, string word2) {
         if(word1.length()==0)
@@ -35,6 +36,10 @@ public:
         if(word2.length()==0)
             return word1.length();
         
-        return solveTab(word1,word2,word1.length(),word2.length());
+        int x=word1.length();
+        int y=word2.length();
+        dp.resize(x,vector<int>(y,-1));
+        
+        return solve(0,0,word1,word2,x,y);
     }
 };
