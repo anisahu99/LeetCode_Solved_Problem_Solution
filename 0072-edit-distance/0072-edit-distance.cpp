df8527349fd38bafbd2dfed3aac403c6da262eba
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-06-23 10:48:19 +0530

    Time: 19 ms (47.84%), Space: 8.9 MB (62.81%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int solveTab(string &s1,string &s2,int x,int y){
-        
-        vector<int> curr(y+1,0);
-        vector<int> next(y+1,0);
+        vector<vector<int>>dp(x+1,vector<int>(y+1,0));
         
         for(int j=0;j<y;j++){
-            next[j]=y-j;
+            dp[x][j]=y-j;
+        }
+        for(int i=0;i<x;i++){
+            dp[i][y]=x-i;
         }
         
         for(int i=x-1;i>=0;i--){
             for(int j=y-1;j>=0;j--){
-                curr[y]=x-i;
                 int ans=0;
                 if(s1[i]==s2[j]){
-                    curr[j]=next[j+1];
+                    dp[i][j]=dp[i+1][j+1];
                 }
                 else{
-                    int insrt=1+curr[j+1];
-                    int rplc=1+next[j+1];
-                    int delt=1+next[j];
+                    int insrt=1+dp[i][j+1];
+                    int rplc=1+dp[i+1][j+1];
+                    int delt=1+dp[i+1][j];
                     
                     ans=min(insrt,min(rplc,delt));
-                    curr[j]=ans;
+                    dp[i][j]=ans;
                 }
+                
             }
-            next=curr;
         }
-        return next[0];
+        return dp[0][0];
     }
     int minDistance(string word1, string word2) {
         if(word1.length()==0)
