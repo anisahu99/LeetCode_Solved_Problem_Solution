62c5d42964e4e112c2a70cdf222b7e8bb30d138c
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-07-01 14:08:41 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (62.54%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -3,12 +3,15 @@ public:
     //int ans = INT_MAX;
     //pick cookies bag or not.
     //brute force
+
     int solve(int pick, vector<int>& nums, vector<int>& v, int k){
         if(pick==nums.size()){
             int mx = INT_MIN;
             for(int i=0;i<k;i++){
                 mx = max(mx,v[i]);
+                //cout<<v[i]<<" ";
             }
+            //cout<<endl;
             return mx;
         }
         int res=INT_MAX;
@@ -16,6 +19,8 @@ public:
             v[i] += nums[pick];
             res=min(res,solve(pick+1,nums,v,k));
             v[i] -= nums[pick];
+            if(v[i]==0)
+                break;
         }
         return res;
     }
@@ -23,7 +28,6 @@ public:
     int distributeCookies(vector<int>& nums, int k) { // nums is the cookies vector
         int n = nums.size();
         vector<int> v(k,0); // v is to store each sum of the k subsets
-        
         return solve(0,nums,v,k);
     }
 };
