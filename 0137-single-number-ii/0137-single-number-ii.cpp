f6456bf5668ea90721a77e26f83e25bfd017596b
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-07-04 14:39:43 +0530

    Time: 11 ms (46.18%), Space: 9.5 MB (41.15%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> bit(32,0);
-        for(int & num:nums){
-            for(int i=0;i<32;i++){
-                if(num&(1<<i)){
-                    bit[i]++;
-                }
-            }
-        }
         int ans=0;
         for(int i=0;i<32;i++){
-            if(bit[i]%3==1){
+            int sum=0;
+            for(int &num:nums){
+                if(num&(1<<i)){
+                    sum++;
+                    sum%=3;
+                }
+            }
+            if(sum%3){
                 ans=ans|(1<<i);
             }
         }
