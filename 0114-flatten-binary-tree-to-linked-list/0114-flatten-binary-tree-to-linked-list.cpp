20d7576652234b8bfe8c7a91c8a0d5dda3d870a7
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-08-06 13:31:12 +0530

    Time: 10 ms (25.05%), Space: 12.6 MB (52.16%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -1,17 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+    TreeNode* help(TreeNode* root){
+        TreeNode* temp=root;
+        TreeNode* ans=temp;
+        while(temp->right){
+            ans=temp->right;
+            temp=temp->right;
+        }
+        return ans;
+    }
+    TreeNode* solve(TreeNode* root){
+        if(!root) return root;
+        // if(!root->left&&!root->right) return root;
+        // cout<<root->val<<" ";
+        TreeNode* l=solve(root->left);
+        TreeNode* r=solve(root->right);
+        root->left=NULL;
+        root->right=NULL;
+        if(!l){
+            root->right=r;
+        }
+        else{
+            root->right=l;
+            TreeNode* leaf=help(l);
+            leaf->right=r;
+        }
+        return root;
+    }
     void flatten(TreeNode* root) {
-        if(root == NULL){
-            return;
-        }
-        flatten(root->left);
-        flatten(root->right);
-        if(root->left){
-            TreeNode *right = root->right;
-            root->right = root->left;
-            root->left = NULL;
-            while(root->right) root = root->right;
-            root->right = right;
-        }
+        solve(root);
     }
 };
