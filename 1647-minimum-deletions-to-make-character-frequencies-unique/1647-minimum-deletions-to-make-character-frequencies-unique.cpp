1974251321e53a22ea56b2b611ffd59254bb58a3
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-08-09 18:20:06 +0530

    Time: 49 ms (83.81%), Space: 14.6 MB (99.04%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    int minDeletions(string s) {
+    int minDeletions(string &s) {
         vector<int> freq(26,0);
-        //map<char,int> mp;
         for(char &ch:s){
             freq[ch-'a']++;
-            //mp[ch]++;
         }
-        // for(auto it:mp){
-        //     cout<<it.first<<"-"<<it.second<<endl;
-        // }
+        sort(freq.rbegin(),freq.rend());
+        
         int cnt=0;
-        bool flag=true;
-        while(flag){
-            flag=false;
-            for(int i=0;i<26;i++){
-                if(freq[i]==0) continue;
-                //cout<<char(i+'a')<<endl;
-                for(int j=0;j<26;j++){
-                    if(i!=j&&freq[i]!=0&&freq[j]!=0&&freq[i]==freq[j]){
-                        freq[j]--;
-                        cnt++;
-                        flag=true;
-                    }
+        for(int i=0;i<26;i++){
+            if(freq[i]==0) continue;
+            for(int j=i+1;j<26;j++){
+                if(freq[j]==0) continue;
+                if(freq[i]==freq[j]){
+                    freq[j]--;
+                    cnt++;
                 }
             }
         }
