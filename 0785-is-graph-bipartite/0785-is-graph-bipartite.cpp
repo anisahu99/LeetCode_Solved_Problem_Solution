f06e641e283394de6d6fab5177cbade4c0f53f47
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-08-18 17:50:03 +0530

    Time: 13 ms (97.68%), Space: 13.6 MB (33.46%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -2,28 +2,22 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
-        queue<pair<int,int>> q;
-        vector<int> vis(n);
         vector<int> color(n,-1);
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                q.push({i,1});
-                color[i]=1;
-            }
-            while(!q.empty()){
-                int node=q.front().first;
-                int col=q.front().second;
-                q.pop();
-                for(int next:graph[node]){
-                    if(!vis[next]){
-                        q.push({next,1-col});
-                        color[next]=1-col;
-                        vis[next]=1;
+            if(color[i]==-1){
+                color[i]=0;
+                q.push({i,0});
+                while(!q.empty()){
+                    int node=q.front().first;
+                    int c=q.front().second;q.pop();
+                    for(int &adj:graph[node]){
+                        if(color[adj]==-1){
+                            color[adj]=1-c;
+                            q.push({adj,1-c});
+                        }
+                        else if(color[adj]==c) return false;
                     }
-                    else if(vis[next]&&color[next]!=(1-col)){
-                        return false;
-                    }
-                    
                 }
             }
         }
