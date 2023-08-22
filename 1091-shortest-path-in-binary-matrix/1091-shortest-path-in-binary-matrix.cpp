4d6b3bd6df14baf2b53889b0f6d25dcdc7a9bf5c
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-08-22 20:48:32 +0530

    Time: 55 ms (82.64%), Space: 19.8 MB (78.76%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,48 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> directions{{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        if(m == 0 || n == 0 || grid[0][0] != 0)
-            return -1;
-        
-        queue<pair<int, int>> que;
-        que.push({0, 0});
-        grid[0][0] = 1;
-        
-        auto isSafe = [&](int x, int y) {
-            return x>=0 && x<m && y>=0 && y<n;
-        };
-        
-        int steps = 1;
-        
-        while(!que.empty()) {
-            
-            int N = que.size();
-            while(N--) {
-
-                auto curr = que.front();
-                que.pop();
-                int x = curr.first;
-                int y = curr.second;
-
-                if(x == m-1 && y == n-1)
-                    return steps;
-
-                for(auto dir:directions) {
-                    int x_ = x + dir[0];
-                    int y_ = y + dir[1];
-
-                    if(isSafe(x_, y_) && grid[x_][y_] == 0) {
-                        que.push({x_, y_});
-                        grid[x_][y_] = 1;
-                    }
+    vector<vector<int>> d{{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
+    int bfs(int &n,vector<vector<int>>& grid){
+        if(grid[0][0]) return -1;
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        grid[0][0]=1;
+        int cnt=1;
+        while(!q.empty()){
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                int row=q.front().first;
+                int col=q.front().second;q.pop();
+                if(row==n-1&&col==n-1) return cnt;
+                
+                for(int k=0;k<8;k++){
+                    int r=row+d[k][0];
+                    int c=col+d[k][1];
+                    if(r<0||c<0||r==n||c==n||grid[r][c]) continue;
+                    grid[r][c]=1;
+                    q.push({r,c});
                 }
             }
-            steps++;
+            cnt++;
         }
-        
         return -1;
+        
+    }
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        return bfs(n,grid);
     }
 };
