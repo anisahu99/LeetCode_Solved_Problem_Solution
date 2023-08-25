5c9358f793ce2c1cbe894eb0990a45186aa8d8b7
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-08-25 20:09:48 +0530

    Time: 3 ms (78.62%), Space: 6.2 MB (90.66%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     int m,n,N;
-    int t[101][101][201];
-    bool solve(int i,int j,int k,string &s1,string &s2,string &s3){
-        if(i==m&&j==n&&k==N) return true;
-        if(k==N) return false;
-        if(t[i][j][k]!=-1) return t[i][j][k];
+    int t[101][101];
+    //i+j=k
+    //by obsevation
+    bool solve(int i,int j,string &s1,string &s2,string &s3){
+        if(i==m&&j==n&&i+j==N) return true;
+        if(i+j==N) return false;
+        if(t[i][j]!=-1) return t[i][j];
         
         bool result=false;
-        if(i<m&&s1[i]==s3[k]){
-            result=solve(i+1,j,k+1,s1,s2,s3);
+        if(i<m&&s1[i]==s3[i+j]){
+            result=solve(i+1,j,s1,s2,s3);
         }
         if(result){
-            return t[i][j][k]=result;
+            return t[i][j]=result;
         }
-        if(j<n&&s2[j]==s3[k]){
-            result=solve(i,j+1,k+1,s1,s2,s3);
+        if(j<n&&s2[j]==s3[i+j]){
+            result=solve(i,j+1,s1,s2,s3);
         }
-        return t[i][j][k]=result;
+        return t[i][j]=result;
     }
     bool isInterleave(string &s1, string &s2, string &s3) {
         m=s1.length(),n=s2.length(),N=s3.length();
         memset(t,-1,sizeof(t));
-        return solve(0,0,0,s1,s2,s3);
+        return solve(0,0,s1,s2,s3);
     }
 };
