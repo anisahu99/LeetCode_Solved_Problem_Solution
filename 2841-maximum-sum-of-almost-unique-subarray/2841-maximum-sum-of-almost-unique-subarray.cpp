0157d4d09249d870efd55236c9ca3d9acb8e70fa
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-09-03 11:40:49 +0530

    Time: 114 ms (50.00%), Space: 65 MB (16.67%) - LeetHub

--- 2841-maximum-sum-of-almost-unique-subarray/2841-maximum-sum-of-almost-unique-subarray.cpp
+++ 2841-maximum-sum-of-almost-unique-subarray/2841-maximum-sum-of-almost-unique-subarray.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    // m-pairwise distinct elements means m distinct element both meaning same because
+    //let k=4 m=3 subarray=[7,3,1,7]
+    // possible distinct pairs (7,3)(7,1)(1,3) total distinct pair is 3
+    // so with m distinct elements we only can make m pairwise distinct elements
     long long maxSum(vector<int>& nums, int m, int k) {
         unordered_map<int,int> mp;
         long long ans=0;
