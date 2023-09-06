3e176c2aaef7b8b2d2153e58ba59220b23dbb773
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-09-06 15:00:30 +0530

    Time: 11 ms (28.46%), Space: 11.4 MB (15.63%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,60 +1,37 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
-
 class Solution {
 public:
-    void help(Node*head,unordered_map<Node*,int> &pos){
-        Node* temp=head;
-        int i=0;
-        while(temp){
-            pos[temp]=i++;
-            temp=temp->next;
-        }
-    }
     Node* copyRandomList(Node* head) {
-        Node* temp=head;
-        Node* cHead=NULL;
-        Node* pre=NULL;
-        unordered_map<Node*,int> pos;
-        help(head,pos);
-        map<pair<int,int>,Node*> mp;
-        int i=0;
-        while(temp){
-            Node* cre=new Node(temp->val);
+        if(!head)
+            return NULL;
+        unordered_map<Node*, Node*> mp;
+        Node* curr = head;
+        Node* prev = NULL;
+        Node* newHead = NULL;
+        while(curr) {
+            Node* temp = new Node(curr->val);
+            mp[curr] = temp;
+            if(newHead == NULL) {
+                newHead = temp;
+                prev = newHead;
+            } else {
+                prev->next = temp;
+                prev = temp;
+            }
+            curr = curr->next;
+        }
+        
+        curr = head;
+        Node* newCurr = newHead;
+        while(curr) {
+            if(curr->random == NULL) {
+                newCurr->random = NULL;
+            } else {
+                newCurr->random = mp[curr->random];
+            }
             
-            mp[{i++,temp->val}]=cre;
-            if(!cHead){
-                cHead=cre;
-            }
-            else{
-                pre->next=cre;
-            }
-            pre=cre;
-            temp=temp->next;
+            newCurr = newCurr->next;
+            curr = curr->next;
         }
-        temp=head;
-        pre=cHead;
-        while(temp){
-            if(temp->random){
-                int idx=pos[temp->random];
-                pre->random=mp[{idx,temp->random->val}];
-            }
-            temp=temp->next;
-            pre=pre->next;
-        }
-        return cHead;
+        return newHead;
     }
 };
