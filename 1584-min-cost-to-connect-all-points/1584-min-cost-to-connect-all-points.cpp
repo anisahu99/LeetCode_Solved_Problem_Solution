6b15b49ce20acef68b2b4fb3c9969bc9b95bd231
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-09-15 11:20:01 +0530

    Time: 1352 ms (16.84%), Space: 176.2 MB (21.03%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,10 +1,39 @@
-
 class Solution {
 public:
+    vector<int> parent;
+    vector<int> rank;
     
+    int find(int i) {
+        if(parent[i] != i)
+            parent[i] = find(parent[i]);
+        
+        return parent[i];
+    }
+    
+    void Union(int x, int y,int p_x,int p_y) {        
+        if(p_x != p_y) {
+            if(rank[p_x] > rank[p_y]) {
+                parent[p_y] = p_x;
+            } else if(rank[p_y] > rank[p_x]) {
+                parent[p_x] = p_y;
+            } else {
+                parent[p_y] = p_x;
+                rank[p_x]++;
+            }
+        }
+    }
+    static bool cmp( vector<int> &v1,vector<int> &v2){
+        return v1[0]<v2[0];
+    }
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n=points.size();
-        vector<vector<pair<int,int>>> adj(n);
+        parent.resize(n);
+        rank.resize(n);
+        for(int i = 0; i<n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+        vector<vector<int>> edges;
         for(int i=0;i<n-1;i++){
             int x1=points[i][0];
             int y1=points[i][1];
@@ -12,30 +41,27 @@ public:
                 int x2=points[j][0];
                 int y2=points[j][1];
                 int wt=abs(x1-x2)+abs(y1-y2);
-                adj[i].push_back({j,wt});
-			    adj[j].push_back({i,wt});
+                edges.push_back({wt,i,j});
             }
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,0});
-        int cost=0;
-        vector<int> vis(n,0);
-        while(!pq.empty()){
-            int d=pq.top().first;
-            int node=pq.top().second;
-            pq.pop();
-            if(vis[node]) {
-                continue;
-            }
-            
-            vis[node] = true;
-            cost+=d;
-            for(pair<int,int> nbr:adj[node]){
-                int adjNode=nbr.first;
-                int wt=nbr.second;
-                pq.push({wt,adjNode});
+        sort(edges.begin(),edges.end(),cmp);
+        
+        int sum=0;
+        for(vector<int>& edge:edges){
+            int p_x=find(edge[1]);
+            int p_y=find(edge[2]);
+            if(p_x!=p_y){
+                Union(edge[1],edge[2],p_x,p_y);
+                sum+=edge[0];
             }
         }
-        return cost;
+        
+        return sum;
     }
 };
+
+
+
+// for(vector<int> &edge:edges){
+        //     cout<<"("<<edge[0]<<","<<edge[1]<<","<<edge[2]<<")"<<endl;
+        // }
