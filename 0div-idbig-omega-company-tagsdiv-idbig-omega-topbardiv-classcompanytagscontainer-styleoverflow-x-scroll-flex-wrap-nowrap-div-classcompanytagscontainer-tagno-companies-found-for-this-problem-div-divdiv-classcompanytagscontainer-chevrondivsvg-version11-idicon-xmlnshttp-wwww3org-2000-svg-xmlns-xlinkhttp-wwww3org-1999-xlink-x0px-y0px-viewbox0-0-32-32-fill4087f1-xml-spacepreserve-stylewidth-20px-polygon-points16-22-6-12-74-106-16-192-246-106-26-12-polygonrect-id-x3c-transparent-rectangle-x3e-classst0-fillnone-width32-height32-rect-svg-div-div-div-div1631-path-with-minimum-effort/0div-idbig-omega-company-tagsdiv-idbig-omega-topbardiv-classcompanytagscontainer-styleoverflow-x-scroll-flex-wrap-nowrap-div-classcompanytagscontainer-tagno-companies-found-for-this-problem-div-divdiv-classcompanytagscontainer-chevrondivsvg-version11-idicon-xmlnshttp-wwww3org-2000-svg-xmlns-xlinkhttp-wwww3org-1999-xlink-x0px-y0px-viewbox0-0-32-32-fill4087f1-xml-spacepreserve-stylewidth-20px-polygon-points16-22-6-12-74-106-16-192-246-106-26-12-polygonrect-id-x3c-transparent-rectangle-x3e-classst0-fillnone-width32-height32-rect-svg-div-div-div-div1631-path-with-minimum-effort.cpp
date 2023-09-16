898db999994e5c1b312479284de70f7f5d76d944
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-09-16 12:41:13 +0530

    Time: 103 ms (85.03%), Space: 20.3 MB (37.48%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort.cpp
@@ -1,83 +1,35 @@
+# define P pair<int,pair<int,int>>
 class Solution {
 public:
-    vector<pair<int,int>> d{{0,1},{0,-1},{1,0},{-1,0}};
-    
-    //
-    // make cell as node so we do observation
-    // grid[row][col]=row*n+col
-    //n is size of column here n=lc.
-    //conver cell to node
-    // striver link to convert cell to node
-    // min 08:00
-    
-    //https://www.youtube.com/watch?v=Rn6B-Q4SNyA
-    
-    
-    // {
-    //     {0,1,2},
-    //     {3,4,5},
-    //     {6,7,8}
-    // }
-    //make weighted graph between these adjacent nodes
-    void makeGraph(int &lr,int &lc,vector<vector<int>>& heights,vector<vector<pair<int,int>>> &g,int &mx){
-        
-        auto isSafe = [&](int x, int y) {
-            return x>=0 && x<lr && y>=0 && y<lc;
-        };
-        for(int row=0;row<lr;row++){
-            for(int col=0;col<lc;col++){
-                mx=max(mx,heights[row][col]);
-                for(int k=0;k<4;k++){
-                    int r=row+d[k].first;
-                    int c=col+d[k].second;
-                    if(isSafe(r,c)){
-                        g[row*lc+col].push_back({r*lc+c,abs(heights[row][col]-heights[r][c])});
-                    }
-                }
-            }
-        }
-    }
-    
-    // we select that edge which cost <=poss_cost
-    bool traverse(int &poss_cost,int &n,vector<vector<pair<int,int>>> &g){
-        queue<int> q;
-        vector<int> vis(n);//take visited array taki ham usi node par na pahuch jaae jaha se aaye the
-        q.push(0);
-        vis[0]=1;
-        while(!q.empty()){
-            int node=q.front();q.pop();
-            if(node==n-1) return true;
-            for(pair<int,int> &edge:g[node]){
-                //take edge a-b which cost <=poss_cost
-                if(edge.second<=poss_cost&&!vis[edge.first]){
-                    q.push(edge.first);
-                    vis[edge.first]=1;
-                }
-            }
-        }
-        return false;
-    }
-    //
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int lr=heights.size(),lc=heights[0].size();
-        int n=lr*lc;//total elements
-        vector<vector<pair<int,int>>> g(n);
-        int mx=INT_MIN;
-        makeGraph(lr,lc,heights,g,mx);
-        
-        int start=0,end=mx;
-        int ans=mx;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(traverse(mid,n,g)){
-                ans=min(ans,mid);
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
+        int m=heights.size(),n=heights[0].size();
+        vector<pair<int,int>> d{{0,1},{0,-1},{1,0},{-1,0}};
+        vector<vector<int>> res(m,vector<int>(n,INT_MAX));
+        res[0][0]=0;
+        priority_queue<P,vector<P>,greater<P>> pq;
+        pq.push({0,{0,0}});
+        auto isSafe=[&](int x,int y){
+            return x>=0&&x<m&&y>=0&&y<n;
+        };
+        while(!pq.empty()){
+            int diff=pq.top().first;
+            int row=pq.top().second.first;
+            int col=pq.top().second.second;pq.pop();
+            
+            if(row==m-1&&col==n-1) return diff;
+            for(int k=0;k<4;k++){
+                int nr=row+d[k].first;
+                int nc=col+d[k].second;
+                if(isSafe(nr,nc)){
+                    int maxDiff=max(diff,abs(heights[row][col]-heights[nr][nc]));
+                    if(res[nr][nc]>maxDiff){
+                        pq.push({maxDiff,{nr,nc}});
+                        res[nr][nc]=maxDiff;
+                    }
+                    
+                }
             }
         }
-        return ans;
-        
+        return res[m-1][n-1];
     }
 };
