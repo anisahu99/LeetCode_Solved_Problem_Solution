094adf54c4210d4ed5da83403d70045039ab2b5d
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-09-19 01:22:49 +0530

    Time: 20 ms (43.73%), Space: 10.6 MB (43.53%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div778-swim-in-rising-water/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div778-swim-in-rising-water.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    typedef pair<int,pair<int,int>> P;
+    vector<pair<int,int>> dir{{1,0},{-1,0},{0,1},{0,-1}};
+    int swimInWater(vector<vector<int>>& grid) {
+        int m=grid.size(),n=grid[0].size();
+        vector<vector<P>> g(m*n);
+        priority_queue<P,vector<P>,greater<P>> pq;
+        int ans=0;
+        pq.push({0,{0,0}});
+        auto isSafe=[&](int x,int y){
+            return x>=0&&x<m&&y>=0&&y<n&&grid[x][y]!=-1;
+        };
+        while(!pq.empty()){
+            int t=pq.top().first;
+            int row=pq.top().second.first;
+            int col=pq.top().second.second;pq.pop();
+            ans=max(ans,t);
+            if(row==m-1&&col==n-1) return ans;
+            int GRD=grid[row][col];
+            grid[row][col]=-1;
+            for(int k=0;k<4;k++){
+                int nr=row+dir[k].first;
+                int nc=col+dir[k].second;
+                if(isSafe(nr,nc)){
+                    pq.push({max(GRD,grid[nr][nc]),{nr,nc}});
+                }   
+            }
+        }
+        return ans;
+    }
+};
