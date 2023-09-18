4691e6b60efe2d06824f34ee178221b40dc3fd4f
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-09-18 23:22:20 +0530

    Time: 259 ms (12.50%), Space: 166.4 MB (87.50%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2856-minimum-array-length-after-pair-removals/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2856-minimum-array-length-after-pair-removals.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2856-minimum-array-length-after-pair-removals/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2856-minimum-array-length-after-pair-removals.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    typedef pair<int,int> P;
     
     int minLengthAfterRemovals(vector<int>& nums) {
         unordered_map<int,int> freq;
         for(int &num:nums){
             freq[num]++;
         }
-        auto compare=[&]( P &p1, P &p2 ){
-        return p1.second<p2.second;
-    };
-        priority_queue<P,vector<P>,decltype(compare)> pq(compare);
+        priority_queue<int> pq;
         for(auto it:freq){
-            pq.push(it);
+            pq.push(it.second);
         }
         while(pq.size()>1){
-            int num1=pq.top().first;
-            int f1=pq.top().second;pq.pop();
+            int f1=pq.top();pq.pop();
             
-            int num2=pq.top().first;
-            int f2=pq.top().second;pq.pop();
+            int f2=pq.top();pq.pop();
             f1--;
             f2--;
             if(f1){
-                pq.push({num1,f1});
+                pq.push(f1);
             }
             if(f2){
-                pq.push({num2,f2});
+                pq.push(f2);
             }
         }
         int ans=0;
-        if(!pq.empty()) ans+=pq.top().second;
+        if(!pq.empty()) ans+=pq.top();
         return ans;
     }
 };
