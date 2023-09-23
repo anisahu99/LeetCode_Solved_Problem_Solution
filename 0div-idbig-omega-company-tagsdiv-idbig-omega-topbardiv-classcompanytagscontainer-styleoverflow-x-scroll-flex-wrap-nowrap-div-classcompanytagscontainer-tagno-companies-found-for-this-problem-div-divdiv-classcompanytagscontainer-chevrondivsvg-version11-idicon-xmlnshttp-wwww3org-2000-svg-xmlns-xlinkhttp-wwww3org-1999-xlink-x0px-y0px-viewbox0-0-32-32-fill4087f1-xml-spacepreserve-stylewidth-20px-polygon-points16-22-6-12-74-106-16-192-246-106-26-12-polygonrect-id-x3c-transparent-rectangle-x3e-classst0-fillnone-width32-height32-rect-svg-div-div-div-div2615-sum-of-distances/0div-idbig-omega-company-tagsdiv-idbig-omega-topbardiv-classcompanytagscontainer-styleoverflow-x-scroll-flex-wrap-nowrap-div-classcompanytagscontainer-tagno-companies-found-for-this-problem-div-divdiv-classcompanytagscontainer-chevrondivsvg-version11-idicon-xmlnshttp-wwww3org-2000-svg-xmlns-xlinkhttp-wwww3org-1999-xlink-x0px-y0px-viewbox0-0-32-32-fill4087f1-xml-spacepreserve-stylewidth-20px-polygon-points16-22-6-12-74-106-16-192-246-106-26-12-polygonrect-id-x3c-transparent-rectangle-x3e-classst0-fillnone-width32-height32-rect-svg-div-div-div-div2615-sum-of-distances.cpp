0ad263e619c65ee1e95c3bea12362ff42077958a
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-09-23 12:48:06 +0530

    Time: 186 ms (95.79%), Space: 121.8 MB (35.68%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2615-sum-of-distances/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2615-sum-of-distances.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        unordered_map<int,vector<int>> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]].push_back(i);
+        }
+        vector<long long> ans(nums.size(),0);
+        for(auto it:mp){
+            vector<int> arr=it.second;
+            int s=arr.size();
+            if(s==1){
+                ans[arr[0]]=0;
+                continue;
+            }
+            vector<long> prefix(s,0);
+            vector<long> suffix(s,0);
+            prefix[0]=arr[0];
+            suffix[s-1]=arr[s-1];
+            for(int i=1;i<s;i++){
+                prefix[i]=prefix[i-1]+arr[i];
+            }
+            for(int i=s-2;i>=0;i--){
+                suffix[i]=arr[i]+suffix[i+1];
+            }
+            
+            for(int i=0;i<s;i++){
+                long long left=0;long long right=0;
+                
+                if(i==0){
+                    right=suffix[i+1]-(s-1-i)*1LL*arr[i];
+                }
+                else if(i==s-1){
+                    left=abs(prefix[i-1]-(i-0)*1LL*arr[i]);
+                }
+                else{
+                    left=abs(prefix[i-1]-(i-0)*1LL*arr[i]);
+                    right=suffix[i+1]-(s-1-i)*1LL*arr[i];
+                }
+                ans[arr[i]]=left+right;
+            }
+        }
+        return ans;
+    }
+};
