b2c2768cc615fc00f837ec24f0ffe21160154633
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-09-28 17:34:54 +0530

    Time: 44 ms (5.31%), Space: 44.2 MB (5.06%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div799-champagne-tower/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div799-champagne-tower.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        // if(poured==0||poured==1&&query_row!=0||query_glass!=0) return (double) 0;
+        // if(poured==1&&query_row==0&&query_glass==0) return (double) 1;
+        //if((100*101)/2<=poured) return (double) 1;
+        vector<vector<double>> glass(100);
+        for(int i=0;i<100;i++){
+            glass[i].resize(i+1);
+        }
+        glass[0][0]=poured;
+        
+        for(int row=1;row<100;row++){
+            vector<double> extra(row);
+            for(int col=0;col<row;col++){
+                if(glass[row-1][col]>(double) 1){
+                    double parts=glass[row-1][col]-1;
+                    double divide=parts/2;
+                    extra[col]=divide;
+                    glass[row-1][col]=(double) 1;
+                }
+            }
+            // for(double val:extra){
+            //     cout<<val<<", ";
+            // }
+            
+            // for(double val:glass[row]){
+            //     cout<<val<<", ";
+            // }
+            // cout<<endl;
+            for(int col=0;col<row+1;col++){
+                if(col-1>=0){
+                    glass[row][col]+=extra[col-1];
+                }
+                if(col<extra.size()){
+                    glass[row][col]+=extra[col];
+                }
+            }
+            // for(double val:glass[row]){
+            //     cout<<val<<", ";
+            // }
+            // cout<<"next"<<endl;
+        }
+        
+        for(int col=0;col<100;col++){
+            if(glass[100-1][col]>(double) 1){
+                glass[100-1][col]=(double) 1;
+            }
+        }
+        return glass[query_row][query_glass];
+    }
+};
