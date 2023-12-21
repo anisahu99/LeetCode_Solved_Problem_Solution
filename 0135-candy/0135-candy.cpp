160612ac9de58aeac3b0ddbcfcde3d5b348cbc65
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-12-21 23:17:14 +0530

    Time: 28 ms (10.25%), Space: 20.7 MB (5.31%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    
-    int solve(vector<int> &ratings){
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        int n=ratings.size();
-        vector<int> candy(n,1);
-        for(int i=0;i<n;i++){
-            pq.push({ratings[i],i});
-        }
-        while(!pq.empty()){
-            int idx=pq.top().second;
-            pq.pop();
-            if(idx-1>=0&&ratings[idx-1]<ratings[idx]){
-                candy[idx]=max(candy[idx],1+candy[idx-1]);
-            }
-            if(idx+1<n&&ratings[idx]>ratings[idx+1]){
-                candy[idx]=max(candy[idx],1+candy[idx+1]);
-            }
-        }
-        int total_candy=0;
-        for(int c:candy){
-            total_candy+=c;
-        }
-        return total_candy;
-    }
+    // myself
     int candy(vector<int>& ratings) {
-        return solve(ratings);
+        int n=ratings.size();
+        vector<pair<int,int>> order;
+        for(int i=0;i<n;i++){
+            order.push_back({ratings[i],i});
+        }
+        sort(order.begin(),order.end());
+        // sort rating
+        vector<int> res(n);
+        // distribute in ascending order
+        // if child`s rating greater than neighbouring child`s rating then we give extra one
+        for(int j=0;j<n;j++){
+            int rating=order[j].first;
+            int idx=order[j].second;
+            int candy=0;
+            if(idx-1>=0&&rating>ratings[idx-1]){
+                candy=max(candy,res[idx-1]);
+            }
+            if(idx+1<n&&rating>ratings[idx+1]){
+                candy=max(candy,res[idx+1]);
+            }
+            res[idx]=1+candy;
+        }
+        // count all the given candies
+        int count=0;
+        for(int &c:res){
+            count+=c;
+        }
+        return count;
     }
 };
